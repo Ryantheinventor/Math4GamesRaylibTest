504a3d879529b9e4d4f7d6de047f5bda47a07af0
Ryan Millen <ryantheinventor@gmail.com>
2020-10-16 14:15:37 -0700

    Init Commit and sprite functionality

---
+++ raygamecsharp/Program.cs
@@ -0,0 +1,110 @@
+﻿/*******************************************************************************************
+*
+*   raylib [core] example - Basic window
+*
+*   Welcome to raylib!
+*
+*   To test examples, just press F6 and execute raylib_compile_execute script
+*   Note that compiled executable is placed in the same folder as .c file
+*
+*   You can find all basic examples on C:\raylib\raylib\examples folder or
+*
+*   Enjoy using raylib. :)
+*
+*   This example has been created using raylib-cs 3.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   This example was lightly modified to provide additional 'using' directives to make
+*   common math types and utilities readily available, though they are not using in this sample.
+*
+*   Copyright (c) 2019-2020 Academy of Interactive Entertainment (@aie_usa)
+*   Copyright (c) 2013-2016 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+using static Raylib_cs.Raylib;  // core methods (InitWindow, BeginDrawing())
+using static Raylib_cs.Color;   // color (RAYWHITE, MAROON, etc.)
+using static Raylib_cs.Raymath; // mathematics utilities and operations (Vector2Add, etc.)
+using MathClasses;
+//using System.Numerics;          // mathematics types (Vector2, Vector3, etc.)
+using System.Collections.Generic;
+using Raylib_cs;
+using System;
+
+namespace M4GVisualTest
+{
+    public class core_window
+    {
+
+        public static Dictionary<string, Texture2D> textures = new Dictionary<string, Texture2D>();
+        public static List<Sprite> objects = new List<Sprite>();
+
+        public static int Main()
+        { 
+            const int screenWidth = 1600;
+            const int screenHeight = 900;
+            InitWindow(screenWidth, screenHeight, "Math4Games");
+            SetTargetFPS(60);
+            LoadTextures();
+
+            objects.Add(new Sprite(textures["MainShipPart"], new List<Sprite> { 
+                new Sprite(textures["MainShipPart"],
+                new List<Sprite> {
+                    new Sprite(textures["MainShipPart"]) 
+                }) 
+            }));
+
+            objects[0].transform.m7 = 100;
+            objects[0].transform.m8 = 100;
+            objects[0].children[0].transform.m7 = 100;
+            objects[0].children[0].children[0].transform.m8 = 100;
+
+            // Main game loop
+            while (!WindowShouldClose())    // Detect window close button or ESC key
+            {
+                
+                Update();
+                objects[0].transform.SetRotateZ(0.1f);
+                
+                Draw();
+                
+            }
+
+            CloseWindow();        // Close window and OpenGL context
+            return 0;
+        }
+
+        public static void Update() 
+        {
+            foreach (Sprite sprite in objects)
+            {
+                sprite.Update();
+            }
+        }
+
+
+        public static void Draw() 
+        {
+            BeginDrawing();
+            ClearBackground(BLACK);
+
+            foreach (Sprite sprite in objects) 
+            {
+                sprite.Draw();
+            }
+
+
+            
+
+            EndDrawing();
+        }
+
+        public static void LoadTextures() 
+        {
+            textures.Add("MainShipPart",LoadTexture("Textures/Parts/spaceParts_037.png"));
+        }
+
+
+
+    }
+}
---
+++ raygamecsharp/Sprite.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Raylib_cs;
+using static Raylib_cs.Raylib;  // core methods (InitWindow, BeginDrawing())
+using static Raylib_cs.Color;   // color (RAYWHITE, MAROON, etc.)
+using static Raylib_cs.Raymath; // mathematics utilities and operations (Vector2Add, etc.)
+using MathClasses;
+using System.Numerics;
+
+namespace M4GVisualTest
+{
+    public class Sprite
+    {
+        public Texture2D texture;
+        public Matrix3 transform = new Matrix3();
+        public List<Sprite> children = new List<Sprite>();
+        public Sprite parent = null;
+
+        public Sprite(Texture2D texture) 
+        {
+            this.texture = texture;
+        }
+
+        public Sprite(Texture2D texture, List<Sprite> children)
+        {
+            this.texture = texture;
+            this.children = children;
+            foreach (Sprite c in this.children) 
+            {
+                c.parent = this;
+            }
+        }
+
+        public void Update() 
+        {
+
+            foreach (Sprite child in children)
+            {
+                child.Update();
+            }
+        }
+
+        public void Draw()
+        {
+            Rectangle sourceRec = new Rectangle(0, 0, texture.width, texture.height);
+            NPatchInfo patch = new NPatchInfo();
+            patch.sourceRec = sourceRec;
+
+            Matrix3 worldMatrix = GetWorldMatrix();
+
+            float width = (float)Math.Sqrt(worldMatrix.m1 * worldMatrix.m1 + worldMatrix.m2 * worldMatrix.m2) * texture.width;
+            float height = (float)Math.Sqrt(worldMatrix.m4 * worldMatrix.m4 + worldMatrix.m5 * worldMatrix.m5) * texture.height;
+            float posX = worldMatrix.m7;// - width / 2;
+            float posY = worldMatrix.m8;// - height / 2;
+
+            
+
+
+            float rotation = ((float)Math.Atan2(worldMatrix.m1, worldMatrix.m4))*180/(float)Math.PI;
+            //(float)Math.Acos(0.5 * (worldMatrix.m1 + worldMatrix.m5 + worldMatrix.m9 - 1));
+            Console.WriteLine("----");
+            worldMatrix.PrintMatrixToConsole();
+            Console.WriteLine(rotation);
+            Console.WriteLine("----");
+
+
+            Rectangle destRec = new Rectangle(posX, posY, width, height);
+            //DrawTexture(texture, (int)posX, (int)posY, WHITE);
+            //DrawTexturePro(texture, sourceRec, destRec, new Vector2(width / 2, height / 2), 45, WHITE);
+            DrawTextureNPatch(texture, patch, destRec, new Vector2(width/2, height/2), rotation, WHITE);
+            foreach (Sprite child in children)
+            {
+                child.Draw();
+            }
+        }
+
+
+
+        public Matrix3 GetWorldMatrix() 
+        {
+            if (parent == null)
+            {
+                return transform;
+            }
+            else 
+            {
+                return parent.GetWorldMatrix() * transform;
+            }
+        }
+
+    }
+}
